1b7a410eb0b7e6e38deaee2f42e93f67cb04f7bb
thibaut delporte <71927133+ThibautDelporte98@users.noreply.github.com>
2022-11-18 09:46:25 +0100

    Final commit "Cases"

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = { 
     mode: 'production', 
     watch:true,
-    entry: ['./js/navscroll.js', './js/jobsbutton.js' ,'./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'], 
+    entry: ['./js/cases.js','./js/navscroll.js', './js/jobsbutton.js' ,'./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'], 
     output: { 
         filename: 'main.js', 
         path: path.resolve(__dirname, 'dist'), 
